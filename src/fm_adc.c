282e1877d77f8e6b1a23d67b9987210d3afb5c0a
matt cybanical <cybanical@gmail.com>
2017-02-06 12:17:16 -0500

    fixing some adc things.

--- src/fm_adc.c
+++ src/fm_adc.c
@@ -76,7 +76,7 @@ void MX_ADC1_Init(void)
 	hadc1.Init.ContinuousConvMode = ENABLE;
 	hadc1.Init.DiscontinuousConvMode = DISABLE;
 	hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
-	hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;	//ADC_EXTERNALTRIGCONV_T1_CC1; //
+	hadc1.Init.ExternalTrigConv = ADC_EXTERNALTRIGCONV_T1_CC1; //ADC_SOFTWARE_START;	//ADC_EXTERNALTRIGCONV_T1_CC1; //
 	hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
 	hadc1.Init.NbrOfConversion = ADC_CHANNELS;				// Number of channels
 	hadc1.Init.DMAContinuousRequests = ENABLE;
@@ -100,6 +100,7 @@ void MX_ADC1_Init(void)
 	*/
 	sConfig.Channel = ADC_CHANNEL_1;
 	sConfig.Rank = 2;
+	sConfig.SamplingTime = ADC_SAMPLETIME_15CYCLES;
 	if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
 	{
 		Error_Handler();
@@ -109,6 +110,7 @@ void MX_ADC1_Init(void)
 	*/
 	sConfig.Channel = ADC_CHANNEL_2;
 	sConfig.Rank = 3;
+	sConfig.SamplingTime = ADC_SAMPLETIME_15CYCLES;
 	if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
 	{
 		Error_Handler();
@@ -118,6 +120,7 @@ void MX_ADC1_Init(void)
 	*/
 	sConfig.Channel = ADC_CHANNEL_3;
 	sConfig.Rank = 4;
+	sConfig.SamplingTime = ADC_SAMPLETIME_15CYCLES;
 	if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
 	{
 		Error_Handler();
@@ -127,6 +130,7 @@ void MX_ADC1_Init(void)
 	*/
 	sConfig.Channel = ADC_CHANNEL_4;
 	sConfig.Rank = 5;
+	sConfig.SamplingTime = ADC_SAMPLETIME_15CYCLES;
 	if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
 	{
 		Error_Handler();
@@ -136,6 +140,7 @@ void MX_ADC1_Init(void)
 	*/
 	sConfig.Channel = ADC_CHANNEL_5;
 	sConfig.Rank = 6;
+	sConfig.SamplingTime = ADC_SAMPLETIME_15CYCLES;
 	if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
 	{
 		Error_Handler();
@@ -145,6 +150,7 @@ void MX_ADC1_Init(void)
 	*/
 	sConfig.Channel = ADC_CHANNEL_6;
 	sConfig.Rank = 7;
+	sConfig.SamplingTime = ADC_SAMPLETIME_15CYCLES;
 	if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
 	{
 		Error_Handler();
@@ -154,6 +160,7 @@ void MX_ADC1_Init(void)
 	*/
 	sConfig.Channel = ADC_CHANNEL_7;
 	sConfig.Rank = 8;
+	sConfig.SamplingTime = ADC_SAMPLETIME_15CYCLES;
 	if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
 	{
 		Error_Handler();
